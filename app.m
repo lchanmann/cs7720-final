fbb7a2d4d4005121223b488d8881e3eaa5b77180
Mann <chanmannlim@gmail.com>
2015-05-01 13:49:33 -0500

    experiment nn and dnn with full dataset

--- app.m
+++ app.m
@@ -162,3 +162,87 @@ nn = nntrain(nn, train_x, train_y, opts);
 [er, bad] = nntest(nn, test_x, test_y);
 
 display(er);
+
+%% Experiment with Deep Belief Network with original dataset
+clc; clear all; close all;
+load 'dataset_full.mat'
+
+X = X_full(:, 2:end);
+y = X_full(:, 1);
+[train, test] = data_partition(X, y);
+
+train_x = train(:, 2:end);
+train_y = train(:, 1);
+[r, d] = size(train_x);
+C = unique(train_y)';
+train_y = (train_y * (1 ./ C) == ones(r, length(C)));
+H = 6; %round( (d + length(C)) * 2/3 ); % the number of nodes in each hidden layers
+
+% normalize the data to [0..1]
+% as it is required
+x_min = min(train_x);
+x_max = max(train_x);
+train_x = (train_x - ones(r, 1) * x_min) ./ (ones(r, 1) * (x_max-x_min));
+
+test_x = test(:, 2:end);
+test_y = test(:, 1);
+[r, ~] = size(test_x);
+test_y = (test_y * (1 ./ C) == ones(r, length(C)));
+
+dbn.sizes = [H H]; % hidden nodes of hidden layers
+opts.numepochs = 20;
+opts.batchsize = 1;
+opts.momentum  = 0;
+opts.alpha     = 1; % Learning rate
+dbn = dbnsetup(dbn, train_x, opts);
+dbn = dbntrain(dbn, train_x, opts);
+
+%%unfold dbn to nn
+nn = dbnunfoldtonn(dbn, length(C));
+nn.activation_function = 'sigm';
+nn.learningRate = 1; % Should decrease over time.
+nn.scaling_learningRate = 0.999;
+
+%train nn
+opts.numepochs = 12;
+opts.batchsize = 1;
+nn = nntrain(nn, train_x, train_y, opts);
+[er, bad] = nntest(nn, test_x, test_y);
+
+display(er);
+
+%% Experiment with Neural Networks with original dataset
+clc; clear all; close all;
+load 'dataset_full.mat'
+
+X = X_full(:, 2:end);
+y = X_full(:, 1);
+[train, test] = data_partition(X, y);
+
+train_x = train(:, 2:end);
+train_y = train(:, 1);
+[r, d] = size(train_x);
+C = unique(train_y)';
+train_y = (train_y * (1 ./ C) == ones(r, length(C)));
+H = 6; %round( (d + length(C)) * 2/3 ); % the number of nodes in each hidden layers
+
+test_x = test(:, 2:end);
+test_y = test(:, 1);
+[r, ~] = size(test_x);
+test_y = (test_y * (1 ./ C) == ones(r, length(C)));
+
+% normalize
+[train_x, mu, sigma] = zscore(train_x);
+test_x = normalize(test_x, mu, sigma);
+
+nn = nnsetup([d H length(C)]); % nn structure [input, hidden, ..., hidden, output]
+nn.activation_function = 'sigm';
+nn.learningRate = 1; % Should decrease over time.
+nn.scaling_learningRate = 0.999;
+
+opts.numepochs = 110;
+opts.batchsize = 1; 
+[nn, L] = nntrain(nn, train_x, train_y, opts);
+
+[er, bad] = nntest(nn, test_x, test_y);
+display(er);
