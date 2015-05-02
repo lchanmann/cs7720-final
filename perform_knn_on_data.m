837fa4ffb6357365260d90687012d2c2860fa44d
Fernando Torre Mora <torrefernand@gmail.com>
2015-05-01 20:04:05 -0500

    For chrissake!

--- perform_knn_on_data.m
+++ perform_knn_on_data.m
@@ -6,9 +6,11 @@ specimen = dataset(:, 1);   %sample ID within class
 
 % PCA dimension reduction
 M = [1 2 4 5 6];
+accuracy = zeros(max(M), 1);
 for m=M
-    disp(['PCA', num2str(m)]);
     load(['dataset_pca_train_', num2str(m), '.mat'], 'X_pca_train');    
     load(['dataset_pca_', num2str(m), '.mat'], 'X_pca');    
-    learned_test_class = trainKNN(X_pca_train(:, 2:end)', X_pca_train(:, 1)', X_pca(:, 2:end)', 3, X_pca(:, 1)');
+    learned_test_class = Nearest_Neighbor(X_pca_train(:, 2:end)', X_pca_train(:, 1)', X_pca(:, 2:end)', 3);
+    true_test_class = X_pca(:, 1);
+    accuracy(m) = sum(learned_test_class' == true_test_class)/length(true_test_class);
 end
